898f376d72ad37efa7ecfb4c3ab50ca2f5f92df8
Reid Sherman <reidsherman@gmail.com>
2020-09-16 10:02:40 -0700

    add bol table

--- src/components/editable-input.js
+++ src/components/editable-input.js
@@ -7,6 +7,7 @@ const EditableInput = (props) => {
   const [changed, setChanged] = useState(false)
   // let changed = false
   const saveText = (e) => {
+    const oldText = newText
     const t = e.target.value
     // console.log('t', t)
     if (t === newText || t === props.text) {
@@ -16,7 +17,7 @@ const EditableInput = (props) => {
     }
     changeText(t)
     setEditing(false)
-    props.callback && props.callback(t)
+    props.callback && props.callback(t, oldText)
     setChanged(true)
   }
   const inputRef = useRef(null)
@@ -33,11 +34,11 @@ const EditableInput = (props) => {
   }, [isEditing])
 
   return (
-    <span className={`cursor-text text-xl inline-block`}>
+    <span className={`cursor-text text-xl inline-block ${props.style ? 'hover:bg-blue-400' : ''}`}>
       <div className={`${isEditing && 'hidden '} flex flex-col`}>
         <div className={``}  onClick={focusInput}>
           <span className={`pr-3`}  >{changed ? newText : props.text}</span>
-          <Svg className={`cursor-pointer w-4 h-4 inline-block`} html={`<path xmlns="http://www.w3.org/2000/svg" d="M384,224V408a40,40,0,0,1-40,40H104a40,40,0,0,1-40-40V168a40,40,0,0,1,40-40H271.48" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/> <path xmlns="http://www.w3.org/2000/svg" d="M459.94,53.25a16.06,16.06,0,0,0-23.22-.56L424.35,65a8,8,0,0,0,0,11.31l11.34,11.32a8,8,0,0,0,11.34,0l12.06-12C465.19,69.54,465.76,59.62,459.94,53.25Z"/> <path xmlns="http://www.w3.org/2000/svg" d="M399.34,90,218.82,270.2a9,9,0,0,0-2.31,3.93L208.16,299a3.91,3.91,0,0,0,4.86,4.86l24.85-8.35a9,9,0,0,0,3.93-2.31L422,112.66A9,9,0,0,0,422,100L412.05,90A9,9,0,0,0,399.34,90Z"/>`}/>
+          <Svg className={`cursor-pointer w-4 h-4 inline-block ${props.style ? 'hidden' : ''}`} html={`<path xmlns="http://www.w3.org/2000/svg" d="M384,224V408a40,40,0,0,1-40,40H104a40,40,0,0,1-40-40V168a40,40,0,0,1,40-40H271.48" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/> <path xmlns="http://www.w3.org/2000/svg" d="M459.94,53.25a16.06,16.06,0,0,0-23.22-.56L424.35,65a8,8,0,0,0,0,11.31l11.34,11.32a8,8,0,0,0,11.34,0l12.06-12C465.19,69.54,465.76,59.62,459.94,53.25Z"/> <path xmlns="http://www.w3.org/2000/svg" d="M399.34,90,218.82,270.2a9,9,0,0,0-2.31,3.93L208.16,299a3.91,3.91,0,0,0,4.86,4.86l24.85-8.35a9,9,0,0,0,3.93-2.31L422,112.66A9,9,0,0,0,422,100L412.05,90A9,9,0,0,0,399.34,90Z"/>`}/>
         </div>
         <small className={`font-bold ${!changed && `hidden`}`}>{props.title}</small>
       </div>
--- src/pages/warehouse-dashboard.js
+++ src/pages/warehouse-dashboard.js
@@ -2,116 +2,81 @@ import React, { useState, useEffect, useCallback, useRef } from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import WebcamCapture from '../components/webcam-capture';
+import EditableInput from '../components/editable-input'
+import Svg from '../components/svg'
+import s from './warehouse-dashboard.module.css'
+import { CSVLink, CSVDownload } from "react-csv";
 
-// import BarcodeReader from 'react-barcode-reader'
-// import BarcodeScannerComponent from "react-webcam-barcode-scanner";
-// const WebcamCapture = ({ callback }) => {
-//   const [deviceId, setDeviceId] = React.useState({});
-//   const [devices, setDevices] = React.useState([]);
-//   const webcamRef = React.useRef(null);
-//   const [imageSrc, setImage] = useState(null)
-//   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+const defaultRows = [
+  ['Procter and Gamble', 'L\'Oreal', '7/1/2020', '100 cases'],
+  ['', '', '7/15/2020', '50 cases'],
+  ['', 'Head and Shoulders', '6/1/2020', '10 cases'],
+  ['', 'Dove', '5/1/2020', '100 cases'],
+  ['', '', '6/15/2020', '50 cases'],
+  ['', '', '6/30/2020', '50 cases']
+]
 
-//   const handleDevices = React.useCallback(
-//     mediaDevices =>
-//       setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
-//     [setDevices]
-//   );
-//   const reset = () => {
-//     setImage(null)
-//   }
-//   const capture = React.useCallback(
-//     () => {
-//       if (imageSrc) setImage(null)
-//       const imageSrc = webcamRef.current.getScreenshot();
-//       setImage(imageSrc)
-//     },
-//     [webcamRef]
-//   );
-//   let done = false
-
-//   const finished = () => {
-//     done = true
-//     callback(imageSrc)
-//   }
-
-//   React.useEffect(
-//     () => {
-//       navigator.mediaDevices.enumerateDevices().then(handleDevices);
-//     },
-//     [handleDevices]
-//   );
-
-//   function getWindowDimensions() {
-//     if (typeof window == 'undefined') return {}
-//     const { innerWidth: width, innerHeight: height } = window;
-//     // console.log('width, height', width, height)
-//     return {
-//       width,
-//       height
-//     };
-//   }
-
-//   useEffect(() => {
-//     function handleResize() {
-//       setWindowDimensions(getWindowDimensions());
-//     }
-
-//     window.addEventListener('resize', handleResize);
-//     return () => window.removeEventListener('resize', handleResize);
-//   }, []);
-
-//   const cams = devices.map((device, idx) => {
-//     const widthCalc = ((100 / devices.length) - 10) + "%"
-//     const widthCalcPx = ((windowDimensions.width - 100) / devices.length) + "px"
-//     return (
-//       <div key={idx} className={`flex flex-col items-center my-0 mx-auto justify-center ${done ? 'hidden' : ''}`} style={{ width: widthCalc }}>
-//         <div>
-//           <Webcam
-//             ref={webcamRef}
-//             screenshotFormat="image/jpeg"
-//             audio={false}
-//             width={widthCalcPx}
-//             videoConstraints={{ deviceId: device.deviceId }} />
-//           {/* {device.label || `Device ${key + 1}`} */}
-//           <img src={imageSrc} />
-//         </div>
-//         <button onClick={imageSrc ? reset : capture}>{!imageSrc ? 'Capture photo' : 'Retake Photo'}</button>
-//         <button className={`bg-green-500 ${imageSrc ? '' : 'hidden'}`} onClick={finished}>Use Picture</button>
-//       </div>
-//     )
-//   })
-
-//   return (
-//     <div>
-//       {cams}
-//     </div>
-//   )
-// }
+const BolTable = (props) => {
+  const rows = props.rows
+  const changeCell = props.changeCell
+  const tableRows = rows.map((row, idx) => {
+    if (props.editable) {
+      return (
+        <tr key={idx} className={s.editableRows}>
+          <td><EditableInput style={1} text={row[0]} callback={(oldText, newText) => changeCell(0, idx, oldText, newText)} /></td>
+          <td><EditableInput style={1} text={row[1]} callback={(oldText, newText) => changeCell(1, idx, oldText, newText)} /></td>
+          <td><EditableInput style={1} text={row[2]} callback={(oldText, newText) => changeCell(2, idx, oldText, newText)} /></td>
+          <td><EditableInput style={1} text={row[3]} callback={(oldText, newText) => changeCell(3, idx, oldText, newText)} /></td>
+        </tr>
+      )
+    } else {
+      return (
+        <tr key={idx}>
+          <td>{row[0]}</td>
+          <td>{row[1]}</td>
+          <td>{row[2]}</td>
+          <td>{row[3]}</td>
+        </tr>
+      )
+    }
+  })
+  return (
+    <table className={`table-auto ${s.bolTable}`}>
+      <thead>
+        <tr>
+          <th>Shipper</th>
+          <th>Item</th>
+          <th>Batch</th>
+          <th>Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableRows}
+      </tbody>
+    </table>
+  )
+}
 
 const WarehouseDashboard = () => {
   const [step, setStep] = useState(0)
   const [uploadedImage, setUploadedImage] = useState(null)
   const [mediaType, setMediaType] = useState(0)
-  // const [barcode, setBarcode] = useState('no result')
-  // const [webcam, useWebcam] = useState(true)
-  // const handleError = (err) => {
-  //   console.log('err', err)
-  // }
-  // const handleScan = scan => {
-  //   console.log('scan', scan)
-  //   setBarcode(scan)
-  // }
+  // const [rows, setRows] = useState(defaultRows)
+  const [rows, setRows] = useState(defaultRows)
+
+  const changeCell = (row, col, newText, oldText) => {
+    // console.log('idx, newText, oldText', row, col, newText, oldText)
+    const t = rows
+    t[row][col] = newText
+    setRows(t)
+  }
   const handleFileUpload = e => {
     e.persist()
-    // window.foo = e.target.files
     const file = e.target.files[0]
     const blob = URL.createObjectURL(file)
     setUploadedImage(blob)
-    // console.log('e', e.files, e.target.files)
   }
   const useWebcamImage = image => {
-    // console.log('image', image)
     setUploadedImage(image)
     nextStep()
   }
@@ -126,18 +91,39 @@ const WarehouseDashboard = () => {
   const nextStep = () => {
     changeStep(step + 1)
   }
+  const prevStep = () => {
+    changeStep(step - 1)
+  }
+
 
   const changeStep = newStep => {
     setStep(newStep)
   }
+  // const changeCell = (row, col, newText, oldText) => {
+  //   console.log('idx, newText, oldText', row, col, newText, oldText)
+  //   const t = rows
+  //   t[row][col] = newText
+  //   setRows(t)
+  // }
+
+  // const tableRows = rows.map((row, idx) => {
+  //   return (
+  //     <tr key={idx}>
+  //       <td><EditableInput style={1} text={row[0] || '\t'} callback={(oldText, newText) => changeCell(0, idx, oldText, newText)} /></td>
+  //       <td><EditableInput style={1} text={row[1]} callback={(oldText, newText) => changeCell(1, idx, oldText, newText)} /></td>
+  //       <td><EditableInput style={1} text={row[2]} callback={(oldText, newText) => changeCell(2, idx, oldText, newText)} /></td>
+  //       <td><EditableInput style={1} text={row[3]} callback={(oldText, newText) => changeCell(3, idx, oldText, newText)} /></td>
+  //     </tr>
+  //   )
+  // })
 
   return (
     <Layout>
       <Seo title="Warehouse Dashboard" />
       <div className='flex flex-col items-center justify-center mx-auto my-10 w-full'>
-        <h1>Warehouse Dashboard</h1>
+        {/* <h1>Warehouse Dashboard</h1> */}
         <div className={`flex flex-col items-center justify-center mx-auto my-10 w-full ${step !== 0 ? 'hidden ' : ''}`}>
-          <h2 className="font-bold text-2xl">1. Upload your GS1 Bill of Lading (BOL)</h2>
+          <h2 className="font-bold text-2xl mb-8">1. Upload your GS1 Bill of Lading (BOL)</h2>
           <ul>
             <li className="mb-8">
               <button onClick={useFileUpload}>Upload</button>
@@ -157,52 +143,29 @@ const WarehouseDashboard = () => {
 
           </ul>
         </div>
-        <div className={`flex flex-col items-center justify-center mx-auto my-10 w-full ${step !== 1 ? 'hidden ' : ''}`}>
-          <table className="table-auto">
-            <tr>
-              <th>Shipper</th>
-              <th>Item</th>
-              <th>Batch</th>
-              <th>Quantity</th>
-            </tr>
-            <tr>
-              <td>Procter and Gamble</td>
-              <td>L'Oreal</td>
-              <td>7/1/2020</td>
-              <td>100 cases</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>7/15/2020</td>
-              <td>50 cases</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td>Head and Shoulders</td>
-              <td>6/1/2020</td>
-              <td>10 cases</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td>Dove</td>
-              <td>5/1/2020</td>
-              <td>100 cases</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>6/15/2020</td>
-              <td>50 cases</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>6/30/2020</td>
-              <td>50 cases</td>
-            </tr>
-          </table>
+        <div className={`flex flex-col items-center justify-center mx-auto my-10 w-full md:w-3/4 ${step !== 1 ? 'hidden ' : ''}`}>
+          <h2 className="font-bold text-2xl mb-8">2. Update Inventory</h2>
+          <BolTable changeCell={changeCell} rows={rows} editable />
           <small>sample data for demonstration purposes</small>
+          <div className="flex w-full flex-col-reverse md:flex-row mt-8">
+            <button className={`bg-red-500`} onClick={prevStep}>Back</button>
+            <button className={`bg-green-500`} onClick={nextStep}>Continue</button>
+          </div>
+        </div>
+        <div className={`flex flex-col items-center justify-center mx-auto my-10 w-full md:w-3/4 ${step !== 2 ? 'hidden ' : ''}`}>
+          <h2 className="font-bold text-2xl mb-8">3. Verify and Send Updated BOL</h2>
+          <BolTable changeCell={changeCell} rows={rows}/>
+          <h3 className="mb-4">Ensure this updated data is accurate.</h3>
+          <div className="cursor-pointer">
+            <CSVLink data={rows} filename={"gs1-bol-updated-inventory.csv"} target="_blank">
+            <Svg className="w-32 h-32" html={`<path xmlns="http://www.w3.org/2000/svg" d="M336,176h40a40,40,0,0,1,40,40V424a40,40,0,0,1-40,40H136a40,40,0,0,1-40-40V216a40,40,0,0,1,40-40h40" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/><polyline xmlns="http://www.w3.org/2000/svg" points="176 272 256 352 336 272" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/><line xmlns="http://www.w3.org/2000/svg" x1="256" y1="48" x2="256" y2="336" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/>`} />
+            <p>Download as CSV</p>
+            </CSVLink>
+          </div>
+          <div className="flex w-full flex-col-reverse md:flex-row mt-8">
+            <button className={`bg-red-500`} onClick={prevStep}>Revise</button>
+            <button className={`bg-green-500`} onClick={nextStep}>Done</button>
+          </div>
         </div>
       </div>
     </Layout>
